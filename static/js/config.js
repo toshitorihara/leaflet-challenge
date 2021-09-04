11a11cca2f29ebe5661849c3f4484bc5d7f140a8
phenomenon1223 <phenomenon1223@gmail.com>
2021-09-04 11:22:43 -0700

    updated codes

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,3 @@
-// API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1Ijoicm9zYXpodSIsImEiOiJja2ZvbTFvbzEyM2c1MnVwbTFjdmVycXk5In0.71jVP2vD8pBWO2bsKtI48Q";
+// const API_KEY = "pk.eyJ1IjoicGhlbm9tZW5vbjEyMjMiLCJhIjoiY2t0NjBkcmFiMGRydzJubXVpOGp0MDNqdyJ9.36zuMIJjeK5H3521yF7-cA";
+// const API_KEY = "pk.eyJ1IjoiZGVycmlrZGVubmlzIiwiYSI6ImNrZXVkMnJhMjAxNXYyeHA1NmphbG55dXUifQ.tdm1bY7gd-2JjCP3swyY5Q";
